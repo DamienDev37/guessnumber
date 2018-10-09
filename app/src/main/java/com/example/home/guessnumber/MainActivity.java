a94b4505460bdc0f05d2bc1c76d32e12385f074f
Damien <damien.orriere@viacesi.fr>
2018-10-09 13:36:06 +0200

    Update fonctionnel + couleur

--- app/src/main/java/com/example/home/guessnumber/MainActivity.java
+++ app/src/main/java/com/example/home/guessnumber/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.home.guessnumber;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -13,39 +15,45 @@ public class MainActivity extends Activity implements View.OnClickListener {
     String result="";
     String response="";
     TextView textInfo;
-    TextView textRes;
+    Button btnRes;
     int numbMemo=0;
     ArrayList<Double> listresult = new ArrayList<Double>();
     final int listeBouton[] = {
             R.id.imageButton0,R.id.imageButton1,R.id.imageButton2,R.id.imageButton3,R.id.imageButton4,
-            R.id.imageButton5,R.id.imageButton6,R.id.imageButton7,R.id.imageButton8,R.id.imageButton9
+            R.id.imageButton5,R.id.imageButton6,R.id.imageButton7,R.id.imageButton8,R.id.imageButton9,R.id.btnRes
     };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textInfo = findViewById(R.id.textViewTry);
-        textRes = findViewById(R.id.textViewRes);
+        btnRes = findViewById(R.id.btnRes);
         textInfo.setText("");
-        textRes.setText("?");
+        btnRes.setText("?");
         for(int b : listeBouton){
             findViewById(b).setOnClickListener(this);
         }
     }
     public String checkNumber(String result){
+        numbMemo=Integer.parseInt((String)result);
         if(numbMemo<soluc){
             response="+"+ textInfo.getText();
         }else if(numbMemo>soluc){
             response="-"+ textInfo.getText();
         }else{
+            btnRes.setTextColor(Color.parseColor("#8de500"));
             response="BRAVO";
+            soluc=(int)(Math.random() * (100 - 0) + 1) + 0;
         }
         return response;
     }
     public void onClick(View view) {
         Log.d("TEST", "Bouton" + view.getId());
-
-        if(textInfo.length()<2) {
+        if(view.getId() == R.id.btnRes){
+            textInfo.setText("");
+            btnRes.setText("?");
+        }
+        else if(textInfo.length()<2) {
             switch (view.getId()) {
                 case R.id.imageButton0:
                     textInfo.setText(textInfo.getText() + "0");
@@ -79,7 +87,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
                     break;
                 default:
             }
-            textRes.setText(checkNumber((String) textInfo.getText()));
+            btnRes.setText(checkNumber((String) textInfo.getText()));
         }
     }
 
